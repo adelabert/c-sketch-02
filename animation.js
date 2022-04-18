76a0f6de84bb75c6ccaa945f440630d87c2c95a3
Zuphat <suphattra.yoo@gmail.com>
2022-04-19 02:04:11 +0700

    export file

--- animation.js
+++ animation.js
@@ -5,6 +5,9 @@ const canvasSketch = require("canvas-sketch");
   > canvas-sketch sketch.js --output=output/01
 */
 const settings = {
+	name: "generate",
+	prefix: "artwork",
+	suffix: ".draft",
 	dimensions: [600, 600],
 	pixelsPerInch: 300,
 	orientation: "landscape",
@@ -15,15 +18,12 @@ const sketch = () => {
 		context.fillStyle = "black";
 		context.fillRect(0, 0, width, height);
 
-		// Gradient foreground
 		const fill = context.createLinearGradient(0, 0, width, height);
 		fill.addColorStop(0, "cyan");
 		fill.addColorStop("0.3", "magenta");
 		fill.addColorStop(1, "yellow");
 
-		// Fill rectangle
 		context.strokeStyle = fill;
-		// context.strokeStyle = "white";
 
 		context.lineWidth = width * 0.005;
 
@@ -43,21 +43,15 @@ const sketch = () => {
 				x = ix + (w + gap) * i;
 				y = iy + (h + gap) * j;
 
-				console.log(w / 2, h / 2);
 				context.translate((w / 2) * 0.001, (h / 2) * 0.001);
-				// context.rotate((Math.PI / 180) * 45);
-
 				context.beginPath();
-
 				context.rect(x, y, w, h);
 				context.stroke();
-				// context.closePath();
 
 				if (Math.random() > 0.5) {
 					context.beginPath();
 					context.rect(x + off / 2, y + off / 2, w - off, h - off);
 					context.stroke();
-					// context.closePath();
 				}
 			}
 		}
